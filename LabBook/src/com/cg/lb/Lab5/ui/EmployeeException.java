5f96b5ff0521a0f678bc06a176dba4d3f40983c2
VENKATASAIREDDY <venkatasai1478@gmail.com>
2021-03-09 19:21:14 +0530

    LabAssignments

--- LabBook/src/com/cg/lb/Lab5/ui/EmployeeException.java
+++
@@ -1,9 +0,0 @@
-package com.cg.eis.exception;
-
-public class EmployeeException extends Exception{
-	
-	public String toString() {
-		return "(EXCEPTION): Employee Salary must be above 3000";
-	}
-
-}
